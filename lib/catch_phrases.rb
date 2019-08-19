03385f77531a5d060f4030e8579c92f36339e30b
andrew kim <andrewk1216@gmail.com>
2019-08-19 00:35:46 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,5 +1,4 @@
 def mario
-
   phrase = "It's-a me, Mario!"
   puts phrase
 end
@@ -7,4 +6,8 @@ end
 def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
 end
